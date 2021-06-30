215508274b409790676f607dcae3e8055298f9ee
Nyam0622 <dbs_dmswl92@naver.com>
2021-07-01 05:08:31 +0900

    Java 1주차 2번 예상답안

--- A조/1주차/1-2예상답안.java
+++ A조/1주차/1-2예상답안.java
@@ -1,25 +1,13 @@
-import java.util.Scanner;
-
-public class Jeon { 
-	public static void main(String ares[]) {
+public class Jeon {
+	public static void main(String args[]) {
 		
-		Scanner Myscan = new Scanner(System.in);
-		
-		System.out.print("층을 입력하시오.");
-		int n = Myscan.nextInt();
-		
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n+1; j++) {
-				if(i > j) {
-					System.out.print(" ");
-				}
-				else if(i != j) {
-					System.out.print("*");
-				}
+		for(int i = 1; i < 10; i++) {
+			for(int j = 1; j < i+1; j++) {
+				System.out.print( i + "*" + j + "=" + i*j);
+				System.out.print('\t');
 			}
 			System.out.println();
 		}
-		
 		
 	}
 }
